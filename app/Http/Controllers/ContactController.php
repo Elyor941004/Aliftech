9fe403a4b5c313a10e592015de1df63326cc21f1
Elyor941004 <54538502+Elyor941004@users.noreply.github.com>
2021-06-22 16:16:22 +0500

    Update ContactController.php

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\ContactRequest;
+use App\Http\Requests\ContactUdateRequest;
 use App\Models\Contact;
 
 class ContactController extends Controller
@@ -75,7 +76,7 @@ class ContactController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(ContactRequest $request, $id)
+    public function update(ContactUdateRequest $request, $id)
     {
         $model = Contact::find($id);
        $model->name = $request->name;
